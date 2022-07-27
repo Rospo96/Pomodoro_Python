d396c99a0a4a98485a3ed18a7e85dae78c769f5e
Rospo96 <106426832+Rospo96@users.noreply.github.com>
2022-07-27 18:07:07 -0300

    Commiteando todo en master

---
+++ borrador.py
@@ -0,0 +1,3 @@
+import funcion_descanso_5_min
+
+funcion_descanso_5_min.descando_5()
---
+++ funcion_descanso_30_min.py
@@ -0,0 +1,29 @@
+import time
+
+def pomodoro_25():
+    minutos = 0
+    segundos = 0
+    segundos_60 = 60
+    
+    while True:
+        if minutos == 30:
+            print(f"Finalizó el descanso de: {minutos} minutos : {segundos} segundos")
+            break
+                    
+        
+        if segundos <= 59:
+            segundos += 1
+            time.sleep(1)
+            print(minutos,":",segundos)
+        
+    
+        elif segundos == segundos_60:
+            minutos += 1        
+            segundos = 0                    
+            time.sleep(1)
+            print(minutos,":",segundos)
+        
+       
+
+
+pomodoro_25()
